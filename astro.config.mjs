c86b2f71358e94c0ead8e36fd618210e9bc2b633
7underlines <7underlines@gmail.com>
2026-08-31 14:32:58 +0200

    brand

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://alinpr18.github.io',
+  site: 'https://werkstattl.com',
   vite: {
     plugins: [tailwindcss()],
   },
